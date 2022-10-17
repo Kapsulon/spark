a0452aac973a3914457c2df4dd651c06f844303f
BARY Théo <theo.bary@epitech.eu>
2022-10-17 22:48:43 +0200

    ADD: C Header template file

---
+++ templates/cheader.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, %YEAR%
+** %PROJECT_NAME%
+** File description:
+** %DESCRIPTION%
+*/
+
+#ifndef %FILE_NAME%_H_
+    #define %FILE_NAME%_H_
+
+
+#endif /* !%FILE_NAME%_H_ */
